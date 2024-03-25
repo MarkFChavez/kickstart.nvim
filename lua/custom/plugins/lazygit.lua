47f56e883b42c8420a1c54d544aa278ea05b8cac
Mark Chavez <mjfchavez@gmail.com>
2024-03-25 15:44:44 +0800

    chore: add command for viewing git file history

--- lua/custom/plugins/lazygit.lua
+++ lua/custom/plugins/lazygit.lua
@@ -15,5 +15,6 @@ return {
   -- order to load the plugin when the command is run for the first time
   keys = {
     { '<leader>gg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
+    { '<leader>gf', '<cmd>LazyGitFilterCurrentFile<cr>', desc = 'LazyGit view file history' },
   },
 }
